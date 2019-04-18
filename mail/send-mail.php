c0ca6e444fedefe02468755009e408845d279640
ArturHatoka <hameleon2011007@mail.ru>
2019-04-18 11:19:29 +0300

    refactor

--- mail/send-mail.php
+++ mail/send-mail.php
@@ -2,22 +2,26 @@
 $name = $_POST['name'];
 $telephone = $_POST['telephone'];
 $mail = $_POST['mail'];
+$comment = $_POST['comment'];
 
 $name = htmlspecialchars($name);
 $telephone = htmlspecialchars($telephone);
 $mail = htmlspecialchars($mail);
+$comment = htmlspecialchars($comment);
 
 $name = urldecode($name);
 $telephone = urldecode($telephone);
 $mail = urldecode($mail);
+$comment = urldecode($comment);
 
 $name = trim($name);
 $telephone = trim($telephone);
 $mail = trim($mail);
+$comment = trim($comment);
 
 if (mail("INFO@VPOEXPRO.RU",
     "Заявка с сайта",
-    "Имя: ".$name. "\nТелефон: ".$telephone. "\nEmail:\n".$mail))
+    "Имя: ".$name. "\nТелефон: ".$telephone. "\nEmail:\n".$mail. "\nКометарий:".comment))
 {
     header("Location: /#endthx");
 } else {
